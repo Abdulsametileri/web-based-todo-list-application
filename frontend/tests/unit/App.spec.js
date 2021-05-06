5aacffca95061789a15a02847a98da12a703c6b1
Abdulsametileri <sametileri07@gmail.com>
2021-05-06 22:11:17 +0300

    Mocking axios calls for unit test in vue.js side

--- frontend/tests/unit/App.spec.js
+++ frontend/tests/unit/App.spec.js
@@ -1,20 +1,29 @@
 import {shallowMount} from "@vue/test-utils";
+import flushPromises from "flush-promises";
 import App from "@/App";
+
+jest.mock("axios", () => ({
+    get: () => Promise.resolve({ data: { data: [] } }),
+    post: () => Promise.resolve({ data: { data: { id: 0, description: 'dummy todo' }} })
+}));
 
 describe('App.vue', () => {
     it('should add a todo when user enters input and clicks the button', async () => {
-        const wrapper = shallowMount(App)
-
         const inputToAdd = "dummy todo"
+
+        const wrapper = shallowMount(App)
+        await flushPromises();
 
         const input = wrapper.find('input')
         await input.setValue(inputToAdd)
 
         const button = wrapper.find('button')
         await button.trigger('click')
+        await flushPromises();
 
-        expect(wrapper.vm.todoList[0]).toBe(inputToAdd)
         expect(wrapper.vm.todo).toBe('')
+        expect(wrapper.vm.todoList[0].id).toEqual(0)
+        expect(wrapper.vm.todoList[0].description).toBe(inputToAdd)
         expect(wrapper.find('p').text()).toBe('1. ' + inputToAdd)
     })
 })
